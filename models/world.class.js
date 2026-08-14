da8a40616d79567e69e64e76c60e31dac87976c4
Surab Link <surab.l@hotmail.de>
2026-08-14 13:04:13 +0200

    feat: add background object rendering

--- models/world.class.js
+++ models/world.class.js
@@ -11,6 +11,9 @@ class World {
     clouds = [
         new Cloud()
     ];
+    backgroundObjects = [
+        new BackgroundObject("img/5_background/layers/3_third_layer/1.png"),
+    ];
 
     constructor(canvas) {
         this.ctx = canvas.getContext('2d');
@@ -20,32 +23,18 @@ class World {
 
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        this.ctx.drawImage(
-            this.character.img,
-            this.character.x,
-            this.character.y,
-            this.character.width,
-            this.character.height
-        );
+        this.addToMap(this.character);
 
         this.enemies.forEach(enemy => {
-            this.ctx.drawImage(
-                enemy.img,
-                enemy.x,
-                enemy.y,
-                enemy.width,
-                enemy.height
-            );
+            this.addToMap(enemy);
         });
 
         this.clouds.forEach(cloud => {
-            this.ctx.drawImage(
-                cloud.img,
-                cloud.x,
-                cloud.y,
-                cloud.width,
-                cloud.height
-            );
+            this.addToMap(cloud);
+        });
+
+        this.backgroundObjects.forEach((bgo) => {
+            this.addToMap(bgo);
         });
 
         let self = this;
@@ -53,4 +42,15 @@ class World {
             self.draw();
         });
     }
+
+    addToMap(mo) {
+        this.ctx.drawImage(
+            mo.img,
+            mo.x,
+            mo.y,
+            mo.width,
+            mo.height
+        );
+    }
+
 }
