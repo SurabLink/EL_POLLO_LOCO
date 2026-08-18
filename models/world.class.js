ffdaf92ec6b63c6fe7059342a95b9227f7d3b383
Surab Link <surab.l@hotmail.de>
2026-08-18 12:45:45 +0200

    feat: add reusable object rendering to world

--- models/world.class.js
+++ models/world.class.js
@@ -12,7 +12,7 @@ class World {
         new Cloud()
     ];
     backgroundObjects = [
-        new BackgroundObject("img/5_background/layers/3_third_layer/1.png"),
+        new BackgroundObject("img/5_background/layers/3_third_layer/1.png", 0, 100),
     ];
 
     constructor(canvas) {
@@ -24,22 +24,20 @@ class World {
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.addToMap(this.character);
+        this.addObjectsToMap(this.backgroundObjects);
+        this.addObjectsToMap(this.enemies);
+        this.addObjectsToMap(this.clouds);
 
-        this.enemies.forEach(enemy => {
-            this.addToMap(enemy);
-        });
-
-        this.clouds.forEach(cloud => {
-            this.addToMap(cloud);
-        });
-
-        this.backgroundObjects.forEach((bgo) => {
-            this.addToMap(bgo);
-        });
 
         let self = this;
         requestAnimationFrame(function () {
-            self.draw();
+            self.draw(objects);
+        });
+    }
+
+    addObjectsToMap(objects) {
+        objects.forEach(o => {
+            this.addToMap(o);
         });
     }
 
